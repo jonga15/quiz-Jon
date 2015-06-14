4c2d76ac483749ae8519a8c6a0ea37df885c8735
Jon Garrido <jonga15@gmail.com>
2015-06-14 17:20:49 +0200

    múltiples preguntas y autoload

--- models/models.js
+++ models/models.js
@@ -40,8 +40,12 @@ sequelize.sync().success(function () {
             Quiz.create({
                 pregunta: 'Capital de Italia',
                 respuesta: 'Roma'
+            });
+            Quiz.create({
+                pregunta: 'Capital de Portugal',
+                respuesta: 'Lisboa'
             })
-                .success(function () {
+                .then(function () {
                     console.log('Base de datos inicializada')
                 });
         }
